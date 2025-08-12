485dbd3dd1f1e07f6d2cc8e864c80d6c44725213
Bastián <bastianvbp@gmail.com>
2025-08-11 21:15:11 -0300

    cambios guardados

--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#En el htpp response están las respuestas que se devuelven a los request. Por eso se importan y se usan en el return de abajo, devuelve la respuesta
 
 # Create your views here.
+
+def inicio(request):
+    return HttpResponse ("Esta es la página de inicio")
+    
