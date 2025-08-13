98707a08358f25adcfab90d957e2bfea6f217ad8
Bastián <bastianvbp@gmail.com>
2025-08-13 20:02:46 -0300

    Aplicando conceptos de la entrega

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import inicio, crear_auto, listado_de_autos
 
 urlpatterns = [
-    path("inicio/", inicio)
+    path("inicio/", inicio, name="inicio"),
+    path("autos/crear/", crear_auto, name="crear_auto"),
+    path("autos/", listado_de_autos, name="listado_de_autos"),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from inicio.models import Auto
+from inicio.forms import FormularioCrearAuto
 #En el htpp response están las respuestas que se devuelven a los request. Por eso se importan y se usan en el return de abajo, devuelve la respuesta
 
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse ("Esta es la página de inicio")
+
+    return render(request, "inicio.html")
+
+
+def crear_auto(request):
+
+    print(request.POST)
+
+    if request.method == "POST":
+        formulario = FormularioCrearAuto(request.POST)
+        if formulario.is_valid():
+            info = formulario.cleaned_data
+
+            auto = Auto(marca=info.get("marca"), modelo=info.get("modelo"))
+            auto.save()
+
+            return redirect("listado_de_autos")
+    else:
+        formulario = FormularioCrearAuto()
+        
+    return render(request, "crear_auto.html", {"formulario": formulario})
+
+def listado_de_autos(request):
+
+    autos = Auto.objects.all()
+    
+    return render(request, "listado_de_autos.html", {"autos": autos})
     
